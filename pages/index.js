dd443bcc5a2260ecbca8cd67cd76d9e498f34ec0
Masayuki Takahashi <baskemasa1206@gmail.com>
2021-09-23 18:19:04 +0900

    Edit my introduction

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Mozu. I'm a web engineer and work in Tokyo.</p>
+        <p>Hello. I am Mozu, a web engineer working in Tokyo.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
